24f264d52d4600e6c815e00f72ca64ffae938e71
KDars24 <khairnardarshan_22206@aitpune.edu.in>
2025-06-06 13:18:26 +0530

    0/

---
+++ neetcode/trees/balanced_binary_trees.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+    int max_height(TreeNode* root) {
+        if(!root) return 0;
+        return 1 + max(max_height(root->left), max_height(root->right));
+    }
+public:
+    bool isBalanced(TreeNode* root) {
+        if(!root) return true;
+        
+        int left  = max_height(root->left);
+        int right = max_height(root->right);
+
+        if(abs(left - right) > 1) return false;
+        return isBalanced(root->left) && isBalanced(root->right);
+        
+    }
+};
+
